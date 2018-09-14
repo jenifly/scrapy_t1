d9a9d2d96e44a85b46aa620a2088ba3811744828
Jenifly <1439916120@qq.com>
2018-09-14 09:14:26 +0800

    1

--- jenifly_scrapy/middlewares.py
+++ jenifly_scrapy/middlewares.py
@@ -18,15 +18,3 @@ class JavaScriptMiddleware(object):
             return HtmlResponse(driver.current_url, body=body, encoding='utf-8', request=request)
         else:
             return
-
-    def process_request(self, request, spider):
-        if spider.name == "musicj":
-            driver = webdriver.Chrome('G:/Tools/selenium/chromedriver.exe') #指定使用的浏览器
-            driver.get(request.url)
-            driver.switch_to.frame('g_iframe')
-            wait = ui.WebDriverWait(driver, 15)
-            if wait.until(lambda driver: driver.find_element_by_class_name('tit')):
-                body = driver.page_source
-            return HtmlResponse(driver.current_url, body=body, encoding='utf-8', request=request)
-        else:
-            return
